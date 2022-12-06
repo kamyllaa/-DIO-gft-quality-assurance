5d83c7c97d666b4f6e8ea115bc438c0526cea103
kamyllaa <112005494+kamyllaa@users.noreply.github.com>
2022-12-06 12:50:15 -0300

    Add files via upload

---
+++ Desafio - POO/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,35 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{ //"extends" mostra que mentoria é filha de conteúdo
+  
+    private LocalDate data;
+
+    public double calcularXp() {
+        return xp_padrao + 20d;
+    }
+
+
+    public Mentoria () {
+
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+   
+     @Override
+     public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+}
